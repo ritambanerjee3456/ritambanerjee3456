a21bf6ec0a059e17025d1548b109b56a67ea6140
Ritam Banerjee <gulubanerjee2308@gmail.com>
2026-07-28 20:03:51 +0530

    Time: 0 ms (100%), Space: 9.6 MB (5.56%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.c
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.c
@@ -1,39 +1,31 @@
+
 struct ListNode* reverseBetween(struct ListNode* head, int left, int right) {
     int n = 0;
-    struct ListNode *ptr = head;
-
-    while (ptr != NULL) {
+    struct ListNode* ptr = head;
+    while(ptr != NULL){
         n++;
-        ptr = ptr->next;
+        ptr=ptr->next;
     }
-
-    if (n <= 1)
-        return head;
-
+    if(n==1) return head;
     int ar[n];
-
     ptr = head;
-    for (int i = 0; i < n; i++) {
-        ar[i] = ptr->val;
+    for(int i=0;i<n;i++){
+        ar[i]= ptr->val;
         ptr = ptr->next;
     }
-
-    int i = left - 1;
-    int j = right - 1;
-
-    while (i < j) {
-        int temp = ar[i];
-        ar[i] = ar[j];
-        ar[j] = temp;
-        i++;
-        j--;
+    int i=left-1;
+    int j=right-1;
+    while(i<j){
+          int temp = ar[i];
+          ar[i]=ar[j];
+          ar[j]=temp;
+          j--;
+          i++;
     }
-
     ptr = head;
-    for (int i = 0; i < n; i++) {
-        ptr->val = ar[i];
-        ptr = ptr->next;
+    for(int i=0;i<n;i++){
+         ptr->val = ar[i];
+         ptr=ptr->next;
     }
-
     return head;
 }
