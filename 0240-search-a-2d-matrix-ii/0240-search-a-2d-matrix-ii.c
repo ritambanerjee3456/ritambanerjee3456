987eb01d05d79b323ed60d84193769adfe5f030a
Ritam Banerjee <gulubanerjee2308@gmail.com>
2026-07-30 11:17:03 +0530

    Time: 1001 ms (5.03%), Space: 12.7 MB (80.73%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.c
@@ -0,0 +1,12 @@
+
+
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target){
+         for(int i=0;i<matrixSize;i++){
+            for(int j=0;j<matrixColSize[0];j++){
+                if(matrix[i][j]==target){
+                    return true;
+                }
+            }
+         }
+         return false;
+}
