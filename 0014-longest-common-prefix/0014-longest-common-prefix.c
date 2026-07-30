76ce0375392cbe5e71fbb49851a9ee32b5034f93
Ritam Banerjee <gulubanerjee2308@gmail.com>
2026-07-30 12:15:10 +0530

    Time: 0 ms (100%), Space: 8.7 MB (89.92%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -0,0 +1,12 @@
+char* longestCommonPrefix(char** strs, int strsSize) {
+    
+    for(int i=0;strs[0][i] != '\0';i++){
+             for(int j=1;j<strsSize;j++){
+                if(strs[j][i]=='\0'|| strs[j][i]!=strs[0][i]){
+                    strs[0][i]='\0';
+                    return strs[0];
+                }
+             }
+    }
+    return strs[0];
+} 
