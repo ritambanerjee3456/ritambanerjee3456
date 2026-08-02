f28d911ab981f10811de2deb58c80173658efc10
Ritam Banerjee <gulubanerjee2308@gmail.com>
2026-08-02 10:55:01 +0530

    Time: 0 ms (100%), Space: 8.7 MB (64.04%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -0,0 +1,31 @@
+bool isValid(char* s) {
+    char* stack = (char*)malloc(strlen(s)*sizeof(char));
+    int top = -1;
+    for(int i=0;i<strlen(s);i++){
+        if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+            top ++;
+            stack[top] = s[i];
+        }
+        else {
+             if(top == -1)  {
+                return false;
+            }
+            if(stack[top] ==  '(' && s[i] != ')'){
+                return false;
+            }
+             if(stack[top] ==  '[' && s[i] != ']'){
+                return false;
+            }
+             if(stack[top] ==  '{' && s[i] != '}'){
+                return false;
+            }
+           
+            top --;   
+        }
+
+    }
+    if(top == -1){
+        return true;
+    }
+    else return false;
+}
