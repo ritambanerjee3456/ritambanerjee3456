3f33d28e7f37c243f7c0db35e29337e71256e255
Ritam Banerjee <gulubanerjee2308@gmail.com>
2026-08-06 12:12:07 +0530

    Time: 0 ms (100%), Space: 12.2 MB (45.74%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.c
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.c
@@ -2,40 +2,21 @@ struct ListNode* oddEvenList(struct ListNode* head) {
 
     if (head == NULL || head->next == NULL)
         return head;
-        int n=0;
-        struct ListNode* temp = head;
-    while (temp != NULL) {
-    n++;
-    temp = temp->next;
-}    
 
-    int *ar = (int *)malloc(n * sizeof(int)); 
-    int *br = (int *)malloc(n * sizeof(int)); 
+    struct ListNode *odd = head;
+    struct ListNode *even = head->next;
+    struct ListNode *evenHead = even;
 
-    int i = 0, j = 0;
-    struct ListNode *ptr = head;
-    int pos = 1;
-    while (ptr != NULL) {
-        if (pos % 2 == 1)
-            br[j++] = ptr->val;
-        else
-            ar[i++] = ptr->val;
+    while (even != NULL && even->next != NULL) {
 
-        ptr = ptr->next;
-        pos++;
-    }
-    ptr = head;
-    for (int k = 0; k < j; k++) {
-        ptr->val = br[k];
-        ptr = ptr->next;
-    }
-    for (int k = 0; k < i; k++) {
-        ptr->val = ar[k];
-        ptr = ptr->next;
+        odd->next = even->next;
+        odd = odd->next;
+
+        even->next = odd->next;
+        even = even->next;
     }
 
-    free(ar);
-    free(br);
+    odd->next = evenHead;
 
     return head;
 }
